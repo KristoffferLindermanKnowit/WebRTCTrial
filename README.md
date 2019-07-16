049136aa01d6e669df875abbc930ea4322cab5cf
KristoffferLindermanKnowit <kristoffer.linderman@knowit.se>
2019-07-16 13:33:45 +0200

    Basics

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # WebRTCTrial
-A trial/temp repo for the exploration of a Browser based WebRTC client
+A trial/temp repo for the exploration of a Browser based WebRTC client. Based on the tutorial given at https://www.scaledrone.com/blog/webrtc-tutorial-simple-video-chat/
---
+++ main.js
@@ -0,0 +1,121 @@
+const configuration = {
+    iceServers: [{
+        urls: 'stun:stun.l.google.com:19302' // Google's public STUN server
+    }]
+};
+
+function onSuccess() {
+    console.log("Success!");
+}
+
+function onError(error) {
+    console.log(error);
+}
+
+// Room name needs to be prefixed with 'observable-'
+const roomName = 'observable-' + "knowit";
+let room;
+const drone = new ScaleDrone('oRT84h7cQKjgjTnN');
+
+drone.on('open', error => {
+    if (error) {
+        return onError(error);
+    }
+    room = drone.subscribe(roomName);
+    room.on('open', error => {
+        if (error) {
+            onError(error);
+        }
+    });
+    // We're connected to the room and received an array of 'members'
+    // connected to the room (including us). Signaling server is ready.
+    room.on('members', members => {
+        if (members.length >= 3) {
+            return alert('The room is full');
+        }
+        // If we are the second user to connect to the room we will be creating the offer
+        const isOfferer = members.length === 2;
+        startWebRTC(isOfferer);
+        startListentingToSignals();
+    });
+});
+
+// Send signaling data via Scaledrone
+function sendMessage(message) {
+    drone.publish({
+        room: roomName,
+        message
+    });
+}
+
+let peerConnection;
+
+function startWebRTC(isOfferer) {
+    peerConnection = new RTCPeerConnection(configuration);
+
+    // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
+    // message to the other peer through the signaling server
+    peerConnection.onicecandidate = event => {
+        if (event.candidate) {
+            sendMessage({
+                'candidate': event.candidate
+            });
+        }
+    };
+
+    // If user is offerer let the 'negotiationneeded' event create the offer
+    if (isOfferer) {
+        peerConnection.onnegotiationneeded = () => {
+            peerConnection.createOffer().then(localDescCreated).catch(onError);
+        }
+    }
+
+    // When a remote stream arrives display it in the #remoteVideo element
+    peerConnection.onaddstream = event => {
+        remoteVideo.srcObject = event.stream;
+    };
+
+    navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+    }).then(stream => {
+        // Display your local video in #localVideo element
+        localVideo.srcObject = stream;
+        // Add your stream to be sent to the conneting peer
+        peerConnection.addStream(stream);
+    }, onError);
+}
+
+function startListentingToSignals() {
+    // Listen to signaling data from Scaledrone
+    room.on('data', (message, client) => {
+        // Message was sent by us
+        if (!client || client.id === drone.clientId) {
+            return;
+        }
+        if (message.sdp) {
+            // This is called after receiving an offer or answer from another peer
+            peerConnection.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
+                // When receiving an offer lets answer it
+                if (peerConnection.remoteDescription.type === 'offer') {
+                    peerConnection.createAnswer().then(localDescCreated).catch(onError);
+                }
+            }, onError);
+        } else if (message.candidate) {
+            // Add the new ICE candidate to our connections remote description
+            peerConnection.addIceCandidate(
+                new RTCIceCandidate(message.candidate), onSuccess, onError
+            );
+        }
+    });
+}
+
+function localDescCreated(desc) {
+    peerConnection.setLocalDescription(
+        desc,
+        () => sendMessage({
+            'sdp': peerConnection.localDescription
+        }),
+        onError
+    );
+}
